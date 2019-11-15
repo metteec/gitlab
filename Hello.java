7c7989b39a29bce1fe52a59b26eb7e6d3cac5c06
metteec <metteec@go.stockton.edu>
2019-11-15 14:44:18 -0500

    Added a default value

--- Hello.java
+++ Hello.java
@@ -2,6 +2,11 @@ public class Hello
 {
 	public static void main( String argv[] )
 	{
+		String name = "World";
+		if(argv.length != 0)
+		{
+			name = argv[0]
+		}
 		System.out.println( "Hello, " + argv[0] + "!" );
 	}
 }
